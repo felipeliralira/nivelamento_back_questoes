4b46bdb42bb84b640d1866589070b5f7a73e50bb
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-04-08 11:53:44 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,39 @@
+// Felipe Lira de Oliveira - 08/04/2026
+// 3°T.D.S
+
+//  1. Exiba a mensagem "Olá mundo" no console.
+
+console.log("Olá Mundo");
+
+// 2. Crie uma variável com o seu nome e exiba o valor no console.
+
+let nome = "Felipe Lira de Oliveira";
+console.log(nome);
+
+// 3. Some dois números e exiba o resultado
+
+let soma = 1 + 2
+console.log(soma);
+
+// 4. Subtraia 2 numeros e exiba o valor
+
+let tira = 1-2;
+console.log(tira);
+
+// 5. Descubra o resto da divisão de um número por outro
+
+let resto = 4%2;
+console.log(resto)
+
+// 6. Crie uma variável booleana (true false) e exiba seu valor
+
+let kwdsa = true
+console.log(kwdsa);
+
+// 7.Verifique se  um número é maior que outro e exiba o resultado
+
+let maiorQue = 18 < 21;
+console.log(maiorQue);
+
+// 8.
+
